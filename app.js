df0b0de2caa9ec7962ae5624ad5908fae0e3776d
Marcos Mendes <marcos.mendes@live.com>
2016-10-23 15:45:58 -0300

    Let's shake baby!
    Version 1.0 shake.js

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function tremble(mode){
+	switch(mode){
+		case "zigZig":
+			shake.tremble("zigZig");
+			break;
+		case "continous":
+			shake.tremble("continous");
+			break;
+		case "sms":
+			shake.tremble("sms");
+			break;
+		case "badum":
+			shake.tremble("badum");
+			break;
+		case "alarmClock":
+			shake.tremble("alarmClock");
+			break;
+		case "sos":
+			shake.tremble("sos");
+			break;
+		case "intruders":
+			shake.tremble("intruders");
+			break;
+		case "annoying":
+			shake.tremble("annoying");
+			break;
+		case "doorBell":
+			shake.tremble("doorBell");
+			break;
+	}
+}
